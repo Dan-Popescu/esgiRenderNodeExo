cb4d556619f9007bd7b29044fe4b040d61fc17b4
Dan-Popescu <popescu_dan@outlook.com>
2024-02-27 15:31:05 +0100

    Update render.yaml

--- db.js
+++ db.js
@@ -2,11 +2,12 @@ const { Sequelize } = require("sequelize");
 
 // database
 const sequelize = new Sequelize(
-  process.env.POSTGRESS_DATABASE_NAME, // Database name
-  process.env.POSTGRESS_DATABASE_USERNAME, // User
-  process.env.POSTGRESS_DATABASE_PASSWORD, // Password
+  // process.env.POSTGRESS_DATABASE_NAME, // Database name
+  // process.env.POSTGRESS_DATABASE_USERNAME, // User
+  // process.env.POSTGRESS_DATABASE_PASSWORD, // Password
+  process.env.DB_CONNECTION_STRING,
   {
-    host: process.env.POSTGRESS_DATABASE_HOST, // Host
+    // host: process.env.POSTGRESS_DATABASE_HOST, // Host
     dialect: "postgres",
     dialectOptions: {
       ssl: {
